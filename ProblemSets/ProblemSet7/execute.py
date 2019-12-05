6140b78b936a9e53fedb53b755f1cb10a897e004
jtregde <jmtregde@icloud.com>
2019-12-04 19:36:52 -0500

    Add PS7 .py files

---
+++ ProblemSets/ProblemSet7/execute.py
@@ -0,0 +1,54 @@
+# import packages
+import numpy as np
+import ar1_approx as ar1
+import functions
+
+
+# Set parameters
+beta = 0.95
+sigma = 1.5
+alpha = 1
+rho = 0.75
+sigma_eps = 0.2
+N = 6
+num_sigma = 4
+
+# Create grid for m
+lb_m = 0.4
+ub_m = 2.0
+size_m = 100
+m_grid = np.linspace(lb_m, ub_m, size_m)
+
+# Theoretical sigma of epsilon
+sigma_P = sigma_eps / ((1 - rho ** 2) ** (1 / 2))
+step = (num_sigma * sigma_P) / (N / 2)
+
+# Create grid for P'
+pi, P_grid = ar1.rouwen(rho, alpha, step, N)
+# P_discrete = sim_markov(P_grid, pi, num_draws)
+
+VFtol = 1e-5 # tolerance for distance between V's
+VFdist = 7.0 # initial distance for V
+VFmaxiter = 1000 # maximum iterations allowed
+V = np.zeros(size_m)
+Vstore = np.zeros((size_m, VFmaxiter)) # initialize array for storing V's
+VFiter = 1 # initialize iterations
+V_params = (beta, sigma)
+
+while VFdist > VFtol and VFiter < VFmaxiter:
+    Vstore[:, VFiter] = V
+    TV, optM = functions.bellman_operator(V, m_grid, P_grid, V_params)
+    VFdist = (np.absolute(V - TV)).max()
+    # print('Iteration: ', VFiter, ', distance: ', VFdist)
+    V = TV
+    VFiter += 1
+
+if VFiter < VFmaxiter:
+    print('Value function converged after ', VFiter, ' iterations.')
+
+else:
+    print('Value function did not converge :(')
+
+VF = V
+
+optC = (m_grid - optM) / P_grid
---
+++ ProblemSets/ProblemSet7/functions.py
@@ -0,0 +1,49 @@
+'''
+Define functions to be used for solving my Dynamic Programming Problem
+'''
+
+# Import packages
+import numpy as np
+import pandas as pd
+from scipy import interpolate
+from scipy.optimize import fminbound
+import scipy.optimize as opt
+import numba
+
+
+# Define utility function
+def utility(m, m_prime, P, sigma):
+    '''
+    Utility function
+    '''
+    c = (m - m_prime) / P
+    if sigma == 1:
+        U = np.log(c)
+    else:
+        U = (c ** (1 - sigma)) / (1 - sigma)
+
+    return U
+
+
+
+# Define the endogenous grid operator
+#@numba.jit()
+def bellman_operator(V, m_grid, P_grid, params):
+    beta, sigma = params
+
+    Val_func = interpolate.interp1d(m_grid, V, kind='cubic', fill_value='extrapolate')
+
+    # Initialize array for operator and policy function
+    TV = np.empty_like(V)
+    optM = np.empty_like(TV)
+
+    for i, p in enumerate(P_grid):
+        for j, m in enumerate(m_grid):
+            def objective(m_prime):
+                return - utility(m, m_prime, p, sigma) - beta * Val_func(m_prime)
+            m_prime_star = fminbound(objective, 1e-6, m - 1e-6)
+            optM[j] = m_prime_star
+            TV[j] = - objective(m_prime_star)
+
+
+    return TV, optM
